6dae4ba34d85bd1d04102305e5188e732784894c
Deegan Acevedo <deeganacevedo@my.whccd.edu>
2026-02-06 19:23:40 -0800

    Basic selection options added

--- COMP003A.Assignment4/Program.cs
+++ COMP003A.Assignment4/Program.cs
@@ -4,7 +4,37 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("1. Start/Continue Routine\n2. View Progress\n3. Exit");
+
+            int stepsTaken = 0;
+
+            do
+            {
+                Console.Write("Enter choice: ");
+                char selection = char.Parse(Console.ReadLine());
+                // selection process starts here
+
+                if (selection == '1')
+                {
+                    stepsTaken++;
+                    Console.WriteLine($"Step {stepsTaken} was completed.");
+                }
+                else if (selection == '2')
+                {
+                    Console.WriteLine($"{stepsTaken} step(s) has/have been taken.");
+                }
+                else if (selection == '3')
+                {
+                    Console.WriteLine("Goodbye!");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid selection, try again.");
+                }
+
+            }
+            while (true;
         }
     }
 }
